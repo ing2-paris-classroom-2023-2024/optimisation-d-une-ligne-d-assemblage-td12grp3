6b40db8c3e2c18c88746104c2c7cbf1985cd5ad7
nihoz <groznykh.ninon1502@gmail.com>
2023-11-15 19:42:52 +0100

    changements

--- main.c
+++ main.c
@@ -1,3 +1,41 @@
-//
-// Created by coren on 11/11/2023.
-//
+#include <stdio.h>
+
+void exclusions(){
+    char nomFichier[] = "exclusions"; // ouverture fichier des exclusions
+    FILE *fichier = fopen(nomFichier, "r");
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture d exclusions .\n");
+    }
+}
+void precedences(){
+    char nomFichier[] = "precedences.txt"; // ouverture fichier des precedences
+    FILE *fichier = fopen(nomFichier, "r");
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture des precedences.\n");
+    }
+}
+
+void operations(){
+    char nomFichier[] = "operations.txt"; // ouverture fichier des operations
+    FILE *fichier = fopen(nomFichier, "r");
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture des operation.\n");
+    }
+}
+
+void temps_cycle(){
+    char nomFichier[] = "temps_cycle.txt"; // ouverture fichier du temps de cycle
+    FILE *fichier = fopen(nomFichier, "r");
+    if (fichier == NULL) {
+        printf("Erreur lors de l'ouverture du temps_cycle.\n");
+    }
+}
+
+int main() {
+    exclusions();
+    precedences();
+    operations();
+    temps_cycle();
+    printf("bien");
+    return 0;
+}
