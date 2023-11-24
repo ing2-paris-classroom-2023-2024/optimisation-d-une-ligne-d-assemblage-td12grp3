a17c01c4402e8f4545a4787e3c553f2a60cf4d37
justclaraa <clara.valette25@gmail.com>
2023-11-24 12:23:40 +0100

    structure amélioration

--- header1.h
+++ header1.h
@@ -5,11 +5,7 @@
 #ifndef OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_TD12GRP3_HEADER1_H
 #define OPTIMISATION_D_UNE_LIGNE_D_ASSEMBLAGE_TD12GRP3_HEADER1_H
 
-typedef struct {
-    int som1;
-    int som2;
 
-} Exclu;
 
 typedef struct {
     int * tab;
