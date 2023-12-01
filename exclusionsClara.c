0a11e1a866c6faa976efdf225a968b0075f11b00
Maxence <maxence.ozog@gmail.com>
2023-12-01 14:37:53 +0100

    nouveau code exclusions

--- exclusionsClara.c
+++ exclusionsClara.c
@@ -1,114 +1,117 @@
 #include <stdio.h>
-#include <stdbool.h>
 #include <stdlib.h>
 
-// Structure pour représenter une opération
-typedef struct {
-    int id;
-    char color;
-} Operation;
+// Définir la taille maximale de l'ensemble d'opérations
+#define MAX_OPERATIONS 35
 
-// Matrice d'exclusion
-int **exclusionMatrix;
+// Définir une structure de station
+struct Station {
+    int operations[MAX_OPERATIONS];
+    int taille;
+};
 
-// Liste des opérations
-Operation *operations;
+// Fonction pour lire les opérations à partir d'un fichier
+int lireOperations(char *nom_fichier, int *operations) {
+    FILE *f_operations = fopen(nom_fichier, "r");
+    if (f_operations == NULL) {
+        perror("Erreur lors de la lecture du fichier d'opérations");
+        return -1;
+    }
 
-// Nombre total d'opérations
-int numOperations;
+    int taille = 0;
 
-// Nombre total de couleurs
-int numColors;
+    while (fscanf(f_operations, "%d", &operations[taille]) == 1) {
+        ++taille;
+    }
 
-// Vérifie si une couleur peut être attribuée à un sommet donné
-bool couleurDispoPourSommet(int sommet, char c) {
-    for (int i = 0; i < numOperations; i++)
-        if (exclusionMatrix[sommet][i] && c == operations[i].color)
-            return false;
-    return true;
+    fclose(f_operations);
+
+    return taille;
 }
 
-// Algorithme de coloration de graphe
-void colorationGraphe() {
-    for (int sommet = 0; sommet < numOperations; sommet++) {
-        for (char c = 'A'; c < 'A' + numColors; c++) {
-            if (couleurDispoPourSommet(sommet, c)) {
-                operations[sommet].color = c;
-                break;
+// Fonction pour lire les paires d'opérations à partir d'un fichier
+int lireExclusions(char *nom_fichier, int exclusions[][2]) {
+    FILE *f_exclusions = fopen(nom_fichier, "r");
+    if (f_exclusions == NULL) {
+        perror("Erreur lors de la lecture du fichier d'exclusions");
+        return -1;
+    }
+
+    int taille = 0;
+
+    while (fscanf(f_exclusions, "%d %d", &exclusions[taille][0], &exclusions[taille][1]) == 2) {
+        ++taille;
+    }
+
+    fclose(f_exclusions);
+
+    return taille;
+}
+
+// Fonction pour vérifier si une opération peut être affectée à une station en respectant les contraintes d'exclusion
+int peutAffecter(int operation, struct Station *station, int E[][2], int taille_E) {
+    for (int i = 0; i < station->taille; ++i) {
+        for (int j = 0; j < taille_E; ++j) {
+            if ((station->operations[i] == E[j][0] && operation == E[j][1]) ||
+                (station->operations[i] == E[j][1] && operation == E[j][0])) {
+                return 0; // Ne peut pas affecter
             }
         }
     }
+    return 1; // Peut affecter
 }
 
-// Fonction pour lire les exclusions depuis le fichier
-void readExclusionsFromFile(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Erreur lors de l'ouverture du fichier");
-        exit(EXIT_FAILURE);
-    }
+// Fonction pour affecter une opération à une station disponible
+void affecterOperation(int operation, struct Station *stations, int *nombre_stations, int E[][2], int taille_E) {
+    int stationDisponible = -1;
 
-    int op1, op2;
-    while (fscanf(file, "%d %d", &op1, &op2) == 2) {
-        exclusionMatrix[op1 - 1][op2 - 1] = exclusionMatrix[op2 - 1][op1 - 1] = 1;
-    }
-
-    fclose(file);
-}
-
-// Fonction pour effectuer la coloration et afficher les résultats
-void performColoring() {
-    // Lecture du nombre d'opérations et de couleurs depuis le fichier ou l'utilisateur
-    printf("Entrez le nombre d'opérations : ");
-    scanf("%d", &numOperations);
-
-    printf("Entrez le nombre de couleurs : ");
-    scanf("%d", &numColors);
-
-    // Allocation dynamique de la mémoire
-    exclusionMatrix = (int **)malloc(numOperations * sizeof(int *));
-    operations = (Operation *)malloc(numOperations * sizeof(Operation));
-
-    for (int i = 0; i < numOperations; i++) {
-        exclusionMatrix[i] = (int *)malloc(numOperations * sizeof(int));
-        operations[i].id = i + 1;
-        operations[i].color = '0'; // Initialise à un caractère nul, car '0' ne sera jamais une couleur valide
-    }
-
-    // Initialisation de la matrice d'exclusion
-    for (int i = 0; i < numOperations; i++) {
-        for (int j = 0; j < numOperations; j++) {
-            exclusionMatrix[i][j] = 0;
+    for (int i = 0; i < *nombre_stations; ++i) {
+        if (peutAffecter(operation, &stations[i], E, taille_E)) {
+            stationDisponible = i;
+            break;
         }
     }
 
-    // Lecture des exclusions depuis le fichier
-    readExclusionsFromFile("exclusionExemple.txt");
-
-    // Applique l'algorithme de coloration
-    colorationGraphe();
-
-    // Affiche les couleurs attribuées à chaque opération
-    printf("Coloration des opérations :\n");
-    for (int i = 0; i < numOperations; i++) {
-        printf("Opération %d : Couleur %c\n", operations[i].id, operations[i].color);
+    if (stationDisponible == -1) {
+        // Créer une nouvelle station
+        stations[*nombre_stations].operations[0] = operation;
+        stations[*nombre_stations].taille = 1;
+        ++(*nombre_stations);
+    } else {
+        // Affecter à la station disponible
+        stations[stationDisponible].operations[stations[stationDisponible].taille] = operation;
+        ++stations[stationDisponible].taille;
     }
-
-    // Libération de la mémoire allouée dynamiquement
-    for (int i = 0; i < numOperations; i++) {
-        free(exclusionMatrix[i]);
-    }
-    free(exclusionMatrix);
-    free(operations);
 }
 
 // Fonction principale
-int main() {
-    // Appel du sous-programme pour effectuer la coloration et afficher les résultats
-    performColoring();
+void repartitionOperations() {
+    int V[MAX_OPERATIONS];
+    int taille_V = lireOperations("operations_a_faire.txt", V);
 
-    return 0;
+    int E[MAX_OPERATIONS][2];
+    int taille_E = lireExclusions("exclusion.txt", E);
+
+    // Si la lecture des fichiers a échoué, quitter la fonction
+    if (taille_V == -1 || taille_E == -1) {
+        return;
+    }
+
+    // Initialiser une liste pour représenter les stations de travail
+    struct Station stations[MAX_OPERATIONS];
+    int nombre_stations = 0;
+
+    // Pour chaque opération, affecter à une station disponible
+    for (int i = 0; i < taille_V; ++i) {
+        affecterOperation(V[i], stations, &nombre_stations, E, taille_E);
+    }
+
+    // Afficher la répartition des opérations par station
+    for (int i = 0; i < nombre_stations; ++i) {
+        printf("Station %d: ", i + 1);
+        for (int j = 0; j < stations[i].taille; ++j) {
+            printf("%d ", stations[i].operations[j]);
+        }
+        printf("\n");
+        }
 }
-//
-// Created by clara on 30/11/2023.
-//
